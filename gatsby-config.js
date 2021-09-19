613dcc1e395c9ef524e78d6491176d4e01d14fae
carbl007 <90735092+carbl007@users.noreply.github.com>
2021-09-19 17:46:41 +0930

    Update gatsby-config.js
    Changed the title of the website to represent the sample implementation for report

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Nüber`,
+    description: `Sample implementation of a user story for Nüber`,
+    author: `@carbl007`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
